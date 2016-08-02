bac6c780683406700fee6834697be8257e421cf3
Alexey Ershov <alex4rom@gmail.com>
2016-08-02 08:26:24 +0300

    Draw a square

--- src/main.js
+++ src/main.js
@@ -2,3 +2,10 @@
 
 const CANVAS = document.querySelector('canvas');
 const CTX = CANVAS.getContext('2d');
+
+function drawSquare() {
+  CTX.fillRect(25,25,100,100);
+  CTX.strokeRect(50,50,50,50);
+}
+
+drawSquare();
