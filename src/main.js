b5a91ce13a5fdaf0b08288f9241b80bdbc6977f4
Alexey Ershov <alex4rom@gmail.com>
2016-08-03 08:31:19 +0300

    Add object

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@
 const CANVAS = document.querySelector('canvas');
 const CTX = CANVAS.getContext('2d');
 
+let monster = {
+  NORMAL: 0,
+  SCARED: 1,
+  state: 0
+}
+
 function drawSquare() {
   CTX.fillRect(25,25,100,100);
   CTX.strokeRect(50,50,50,50);
 }
+
+document.addEventListener('keydown', keydownHandler, false);
+document.addEventListener('keyup', keyupHandler, false);
 
 drawSquare();
